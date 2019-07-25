565434b2e6b2b7e69400f9b2c29d2ce66fe8392e
reireias <reireias@gmail.com>
2019-07-25 22:00:17 +0900

    add color picker

--- components/Theme.vue
+++ components/Theme.vue
@@ -20,6 +20,32 @@
         <v-radio label="dark" :value="true"></v-radio>
       </v-radio-group>
     </v-flex>
+    <v-flex text-center xs12>
+      <v-menu
+        v-for="color in colors"
+        :key="color.name"
+        v-model="color.menu"
+        :close-on-content-click="false"
+        offset-x
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn :color="color.name" fab small v-on="on"></v-btn>
+        </template>
+        <v-card>
+          <v-color-picker
+            v-model="color.value"
+            show-swatches
+            flat
+            :dark="dark"
+          ></v-color-picker>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="color.menu = false">Cancel</v-btn>
+            <v-btn color="primary" @click="setColor(color)">OK</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+    </v-flex>
   </v-layout>
 </template>
 
@@ -32,13 +58,36 @@ export default {
   },
   data() {
     return {
-      dark: false
+      dark: false,
+      primaryMenu: false,
+      colors: [
+        {
+          name: 'primary',
+          menu: false,
+          value: '#ffffff'
+        },
+        {
+          name: 'secondary',
+          menu: false,
+          value: '#ffffff'
+        },
+        {
+          name: 'accent',
+          menu: false,
+          value: '#ffffff'
+        }
+      ]
     }
   },
   methods: {
     themeChanged() {
       this.$vuetify.theme.dark = this.dark
       this.setDark(this.dark)
+    },
+    setColor(color) {
+      this.$vuetify.theme.themes.dark[color.name] = color.value
+      this.$vuetify.theme.themes.light[color.name] = color.value
+      color.menu = false
     },
     ...mapActions(['setDark'])
   }
