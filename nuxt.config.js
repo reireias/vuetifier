89cc46c586647c56678ee30736d496a5904b58d9
reireias <reireias@gmail.com>
2019-07-28 07:48:38 +0900

    update favicon path

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,7 @@ export default {
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
       {
         rel: 'stylesheet',
         href:
@@ -83,22 +83,22 @@ export default {
       {
         rel: 'apple-touch-icon',
         sizes: '180x180',
-        href: '/apple-touch-icon.png'
+        href: './apple-touch-icon.png'
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '32x32',
-        href: '/favicon-32x32.png'
+        href: './favicon-32x32.png'
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '16x16',
-        href: '/favicon-16x16.png'
+        href: './favicon-16x16.png'
       },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
+      { rel: 'manifest', href: './site.webmanifest' },
+      { rel: 'mask-icon', href: './safari-pinned-tab.svg', color: '#5bbad5' }
     ]
   },
   env: {
