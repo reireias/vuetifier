0ba66396eee668b3240b992733701377d39673ff
reireias <reireias@gmail.com>
2019-10-02 22:43:55 +0900

    add lazy load

---
+++ test/components/LazySample.test.js
@@ -0,0 +1,20 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import target from '@/components/LazySample'
+
+Vue.use(Vuetify)
+const localVue = createLocalVue()
+localVue.use(Vuetify)
+
+describe('index', () => {
+  it('should be vue instance', () => {
+    const vuetify = new Vuetify()
+    const wrapper = shallowMount(target, {
+      localVue,
+      vuetify
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
