a1433ee3113dcdd98863690ac374f9ca162e981d
reireias <reireias@users.noreply.github.com>
2020-08-03 15:43:01 +0000

    deploy: 4b57f64294ec99db09f5e4dfc6d29933539509ab

--- sw.js
+++ sw.js
@@ -2,28 +2,24 @@ importScripts('/vuetifier/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/vuetifier/_nuxt/00ec06073d1a25caa156.js",
-    "revision": "ffc491e2266a91e6071e4667912bb3e2"
+    "url": "/vuetifier/_nuxt/app.bcb4671.js",
+    "revision": "8315030370fd1a1809f1e40811768a9f"
   },
   {
-    "url": "/vuetifier/_nuxt/48d1a2214b1e6fada3c8.js",
-    "revision": "98305ce1f7f6098209fc6b8a7f8da497"
+    "url": "/vuetifier/_nuxt/commons/app.861dfba.js",
+    "revision": "2b9712811a48947c9ef0b4a55d82f783"
   },
   {
-    "url": "/vuetifier/_nuxt/600da93328c82089c2da.js",
-    "revision": "e435cd5d8ffd40e8f5bade55c32dddec"
+    "url": "/vuetifier/_nuxt/index.eadadef.js",
+    "revision": "816bdc2ee151ed406415eeaaf404ebdc"
   },
   {
-    "url": "/vuetifier/_nuxt/7e48d97e34404c1f1ce7.js",
-    "revision": "9e056b3e09887c52a446c524cd0fbef9"
+    "url": "/vuetifier/_nuxt/runtime.1e14e6a.js",
+    "revision": "96ed91387c53bd9541096d64c517e571"
   },
   {
-    "url": "/vuetifier/_nuxt/adc7f7f57ebb9e619609.js",
-    "revision": "9149d1aea8aa3068b2995a3ff84d0e2f"
-  },
-  {
-    "url": "/vuetifier/_nuxt/d9b1f1ea1edf725f6353.js",
-    "revision": "9688017a839c6fcc95548216e6832392"
+    "url": "/vuetifier/_nuxt/vendors~app.4537504.js",
+    "revision": "8b6b330dfcd01fe83b0eb0f0b264e2f1"
   }
 ], {
   "cacheId": "vuetifier",
